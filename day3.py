5ea1b8951799c4dd1bf058797eb18095dc1b8818
cloudyyyyy <bicongwang@gmail.com>
2018-08-25 03:56:47 +0800

    Day3: Multiple Linear Regression

--- day3.py
+++ day3.py
@@ -1,25 +1,22 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('datasets/studentscores.csv')
+    df = pd.read_csv('datasets/50_Startups.csv')
     X = df.iloc[:, :-1].values
     Y = df.iloc[:, -1].values
+
+    labelencoder = LabelEncoder()
+    X[:, 3] = labelencoder.fit_transform(X[:, 3])
 
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0) 
 
     lr = LinearRegression()
     lr = lr.fit(X_train, Y_train)
 
-    plt.subplot(2, 1, 1)
-    plt.scatter(X_train, Y_train, color='red')
-    plt.plot(X_train, lr.predict(X_train), color='blue')
-
-    plt.subplot(2, 1, 2)
-    plt.scatter(X_test , Y_test, color='red')
-    plt.plot(X_test , lr.predict(X_test), color='blue')
-
-    plt.show()
+    print(lr.predict(X_test))
+    print(Y_test)
